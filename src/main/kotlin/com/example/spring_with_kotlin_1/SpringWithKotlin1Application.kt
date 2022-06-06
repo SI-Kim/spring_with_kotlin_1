be288e11ec603673f26ef7a2f9d5b11e63696519
SI-Kim <skim88.1942@gmail.com>
2022-06-06 16:19:21 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/spring_with_kotlin_1/SpringWithKotlin1Application.kt
@@ -0,0 +1,11 @@
+package com.example.spring_with_kotlin_1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWithKotlin1Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringWithKotlin1Application>(*args)
+}
---
+++ src/test/kotlin/com/example/spring_with_kotlin_1/SpringWithKotlin1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spring_with_kotlin_1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWithKotlin1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
